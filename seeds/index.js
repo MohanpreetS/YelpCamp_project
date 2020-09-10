12ee5c9f0c008a707ed29dfb90bf4c62f8d273c3
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-09-10 13:09:04 -0400

    seeding- adding single camp

--- seeds/index.js
+++ seeds/index.js
@@ -18,3 +18,16 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+
+const seedDB = async () => {
+    const random1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
